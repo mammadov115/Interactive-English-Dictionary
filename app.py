ba952000130d0877e9c6346d83923a58757d4d67
mammadov115 <mehmanmehman182@gmail.com>
2023-11-16 10:58:09 +0400

    recommending the best match

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import json
+from difflib import get_close_matches
+
 
 data = json.load(open("data.json"))
 
 def translate(word):
     word = word.lower()
+    close_matches = get_close_matches(word, data.keys())
     if word in data:
         return data[word]
+    elif len(close_matches) > 0:
+        return f'Did you mean {close_matches[0]}'
+        
     else:
         return "The word doesn't exist. Please double check it"
 
