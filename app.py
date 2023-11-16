153e1c370a088cdedf7020069d33cd5de6be9910
mammadov115 <mehmanmehman182@gmail.com>
2023-11-16 11:04:33 +0400

    confirmation from the user

--- app.py
+++ app.py
@@ -10,7 +10,15 @@ def translate(word):
     if word in data:
         return data[word]
     elif len(close_matches) > 0:
-        return f'Did you mean {close_matches[0]}'
+        yon = input(f'Did you mean {close_matches[0]}? Enter Y if yes, or N if no: ')
+        if  yon == "Y":
+            return data[close_matches[0]]
+        elif yon == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
         
     else:
         return "The word doesn't exist. Please double check it"
